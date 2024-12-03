b8ec7c99cd25f1a847cf7905ebf577cc44c3c70c
manishdevaraj <manishdevaraj03@gmail.com>
2024-12-03 19:43:26 +0530

    check 3

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -16,7 +16,10 @@ const Layout = async({children}:{children:React.ReactNode}) => {
   const currentUser=await getCurrentUser();
 
   console.log("user is un rechable from db");
-  // if(!currentUser) ;
+
+  if(!currentUser) {
+    redirect('/sign-in');
+  }
   
   return currentUser&&(
      <main className="flex h-screen">
